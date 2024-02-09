e3a2b942fa9a8e0484c156705a80e0a991769875
rahmattSera <rahmathidayatullah996@gmail.com>
2024-02-09 21:24:34 +0700

    feat: improvment list product and category

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,4 +1,5 @@
 import CategoryFiltersTemplate1 from "@/components/category-filters/template-1";
+import CategoryFiltersTemplate2 from "@/components/category-filters/template-2";
 import CheckoutFormsTemplate1 from "@/components/checkout-forms/template-1";
 import FooterTemplate1 from "@/components/footer/template-1";
 import HeroSectionTemplate1 from "@/components/hero-sections/template-1";
@@ -13,6 +14,7 @@ import ProductListTemplate4 from "@/components/product-lists/template-4";
 import ProductListTemplate5 from "@/components/product-lists/template-5";
 import ProductListTemplate6 from "@/components/product-lists/template-6";
 import ProductListTemplate7 from "@/components/product-lists/template-7";
+import ProductListTemplate8 from "@/components/product-lists/template-8";
 import ProductOverviewsTemplate1 from "@/components/product-overviews/template-1";
 import ReviewsTemplate1 from "@/components/reviews/template-1";
 import TrustedTemplate1 from "@/components/trusted/template-1";
@@ -33,12 +35,14 @@ const Page = () => {
       {/* <ProductListTemplate1 />
       <ProductListTemplate2 />
       <ProductListTemplate3 /> */}
-      <ProductListTemplate4 />
+      {/* <ProductListTemplate4 /> */}
+      <ProductListTemplate8 />
       {/* <ProductListTemplate5 />
       <ProductListTemplate6 />
       <ProductListTemplate7 /> */}
 
-      <CategoryFiltersTemplate1 />
+      {/* <CategoryFiltersTemplate1 /> */}
+      <CategoryFiltersTemplate2 />
       {/* <CheckoutFormsTemplate1 />
         <ProductOverviewsTemplate1 /> */}
 
