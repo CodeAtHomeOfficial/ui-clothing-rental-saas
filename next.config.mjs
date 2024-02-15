6e7b7c205b9998a7457971ffe77d1c343e7447ed
Fari <fari.msenju@gmail.com>
2024-02-15 16:52:56 +0700

    fix: remove next config

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    basePath: "/ui-clothing-rental-saas",
-    output: "export",
-    reactStrictMode: true,
-  };
-
-export default nextConfig;
